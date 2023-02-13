1fba2eaddab351c4e4a58daf638fffae316940c5
hikmahDikko <dhywis31@gmail.com>
2023-02-13 18:28:41 +0100

    Initial commit

--- setup/setup.test.js
+++ setup/setup.test.js
@@ -6,6 +6,50 @@ let animals = [
     'tiger'
 ];
 
+beforeAll(() => {
+    console.log("Before all test");
+    animals = [
+        'cat',
+        'dog',
+        'bird',
+        'lion',
+        'tiger'
+    ];
+})
+
+beforeEach(() => {
+    console.log("Before each test");
+    animals = [
+        'cat',
+        'dog',
+        'bird',
+        'lion',
+        'tiger'
+    ];
+});
+
+afterEach(()=> {
+    console.log("After each test");
+    animals = [
+        'cat',
+        'dog',
+        'bird',
+        'lion',
+        'tiger'
+    ];
+});
+
+afterAll(() => {
+    console.log("After all test");
+    animals = [
+        'cat',
+        'dog',
+        'bird',
+        'lion',
+        'tiger'
+    ];
+})
+
 describe("tseting animals array", () => {
     it("should add animal to the end of the array", () =>{
         expect(animals[animals.length-1]).toBe("tiger");
@@ -18,6 +62,6 @@ describe("tseting animals array", () => {
 
     it("should add animal to the beginning of the array", () =>{
         animals.unshift("rabbit");
-        expect(animals[animals.length-1]).toBe("tiger");
+        expect(animals[0]).toBe("rabiit");
     })
 })
